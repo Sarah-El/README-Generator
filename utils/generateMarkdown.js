95123ebbbfa6bdc6acdb8bceb3bdcf9d964539f7
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-01-30 14:46:15 +0000

    created function to generate markdown using user answers to questions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,6 +2,39 @@
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+  - [Description] (#description)
+  - [Installation] (#installation)
+  - [Usage] (#usage)
+  - [Contributing] (#contributing)
+  - [Tests] (#testing)
+  - [Questions] (#questions)
+
+  ## Description
+  ${data.description}
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Tests
+  ${data.testing}
+
+  ## Questions
+  For questions about this application please reach out to me using one of the following methods:
+
+  GitHub: ${data.github}
+  Email: ${data.email}
+
+
 `;
 }
 
