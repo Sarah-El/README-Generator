3d88c33744832ec95334da59017f177426431ae2
Sarah-El <sarah-louise.ellis@hotmail.co.uk>
2023-01-30 12:28:57 +0000

    added function to write README file and updated questions

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const questions = [
     {
         type: "input",
         name: "title",
-        message: "What is your project called?",
+        message: "What is the name of your project?",
       },
       {
         type: "input",
@@ -19,53 +19,7 @@ const questions = [
       {
         type: "input",
         name: "installation",
-        message: "Which steps should be taken to install your project?",
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "What type of license do you have?",
-        choices: [
-          "Apache 2.0 License",
-          "Boost Software License 1.0",
-          "BSD 3-Clause License",
-          "BSD 2-Clause License",
-          "CC0",
-          "CC0 1.0",
-          "Attribution 4.0 International",
-          "CC BY 4.0",
-          "Attribution-ShareAlike 4.0 International",
-          "CC BY-SA 4.0",
-          "Attribution-NonCommercial 4.0 International",
-          "CC BY-NC 4.0",
-          "Attribution-NoDerivates 4.0 International",
-          "CC BY-ND 4.0",
-          "Attribution-NonCommmercial-ShareAlike 4.0 International",
-          "CC BY-NC-SA 4.0",
-          "Attribution-NonCommercial-NoDerivatives 4.0 International",
-          "CC BY-NC-ND 4.0",
-          "Eclipse Public License 1.0",
-          "GNU GPL v3",
-          "GNU GPL v2",
-          "GNU AGPL v3",
-          "GNU LGPL v3",
-          "GNU FDL v1.3",
-          "The Hippocratic License 2.1",
-          "The Hippocratic License 3.0",
-          "IBM Public License Version 1.0",
-          "ISC License (ISC)",
-          "MIT",
-          "Mozilla Public License 2.0",
-          "Attribution License (BY)",
-          "Open Database License (ODbL)",
-          "Public Domain Dedication and License (PDDL)",
-          "The Perl License",
-          "The Artistic License 2.0",
-          "SIL Open Font License 1.1",
-          "Unlicense",
-          "WTFPL",
-          "Zlib",
-        ],
+        message: "What are the installation requirements?",
       },
       {
         type: "input",
@@ -75,7 +29,7 @@ const questions = [
       {
         type: "input",
         name: "contributing",
-        message: "Who and what third-party assets contributed to this project?",
+        message: "What contributed to this project?",
       },
       {
         type: "input",
@@ -83,9 +37,21 @@ const questions = [
         message: "How should this project be tested?",
       },
       {
+        type: "list",
+        name: "license",
+        message: "Which license covers the application?",
+        choices: [
+          "Apache 2.0",
+          "BSD 3",
+          "GPL 3.0",
+          "MIT",
+          "None"
+        ],
+      },
+      {
         type: "input",
         name: "github",
-        message: "What is your GitHub Username?",
+        message: "What is your GitHub username?",
       },
       {
         type: "input",
@@ -96,6 +62,8 @@ const questions = [
 
 // function to write README file
 function writeToFile(fileName, data) {
+    let currentDirectory = path.join(process.cwd(), fileName);
+    fs.writeFileSync (currentDirectory, data);
 }
 
 // function to initialize program
